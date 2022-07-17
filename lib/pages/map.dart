fa239eb03ceda68210db5d25438e6724b2b7b079
rskvprd <vyackov2001@mail.ru>
2022-07-17 18:40:35 +0300

    FEATURE: add marker clusters

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 
 final LatLng mainSmokeRoomCoords = LatLng(55.66965, 37.47935);
@@ -10,6 +11,7 @@ final LatLngBounds mainSmokeRoomBounds =
 
 List<LatLng> _latLngList = [
   LatLng(55.669684, 37.478832),
+  LatLng(55.669684, 37.479),
 ];
 
 List<Marker> _markers = _latLngList
@@ -32,9 +34,10 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
+          plugins: <MapPlugin>[MarkerClusterPlugin()],
           center: mainSmokeRoomCoords,
-          zoom: 18.0,
-          maxZoom: 18,
+          zoom: 18.5,
+          maxZoom: 19,
           minZoom: 1,
           // Paris
           bounds: mainSmokeRoomBounds,
@@ -46,7 +49,7 @@ class MapPage extends StatelessWidget {
           rotation: 0,
           interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
           enableScrollWheel: true,
-          scrollWheelVelocity: 0.005,
+          // scrollWheelVelocity: 0.005, // for newer version
           onTap: (tapPosition, LatLng latLng) {
             // TODO: create onTap
             print(
@@ -56,15 +59,36 @@ class MapPage extends StatelessWidget {
       layers: [
         TileLayerOptions(
           minZoom: 1,
-          maxZoom: 18,
+          maxZoom: 19,
           backgroundColor: Colors.black,
           urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
           subdomains: ['a', 'b', 'c'],
-          userAgentPackageName: 'com.in_pack.usmangroup',
+          // userAgentPackageName: 'com.in_pack.usmangroup', // for newer version
         ),
-        MarkerLayerOptions(
+        // MarkerLayerOptions(
+        //   markers: _markers,
+        // ),
+        MarkerClusterLayerOptions(
+          maxClusterRadius: 190,
+          disableClusteringAtZoom: 16,
+          size: Size(50, 50),
+          fitBoundsOptions: FitBoundsOptions(
+            padding: EdgeInsets.all(50),
+          ),
           markers: _markers,
-        )
+          polygonOptions: PolygonOptions(
+              borderColor: Colors.blueAccent,
+              color: Colors.black12,
+              borderStrokeWidth: 3),
+          builder: (context, markers) {
+            return Container(
+              alignment: Alignment.center,
+              decoration:
+                  BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+              child: Text('${markers.length}'),
+            );
+          },
+        ),
       ],
     );
   }
